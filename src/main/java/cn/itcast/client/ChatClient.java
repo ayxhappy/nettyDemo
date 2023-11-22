98a9aca52f9f1092428a20cfd87bc55a3961f904
anyuxin <15935272187@163.com>
2023-11-22 22:28:30 +0800

    netty聊天室Demo 完成登录流程

---
+++ src/main/java/cn/itcast/client/ChatClient.java
@@ -0,0 +1,115 @@
+package cn.itcast.client;
+
+import cn.itcast.message.LoginRequestMessage;
+import cn.itcast.message.LoginResponseMessage;
+import cn.itcast.protocol.MessageCodecSharable;
+import cn.itcast.protocol.ProcotolFrameDecoder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Slf4j
+public class ChatClient {
+    public static void main(String[] args) {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+        MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        AtomicBoolean LOGIN_FLAG = new AtomicBoolean(false);
+
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(group);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new ProcotolFrameDecoder());
+                    ch.pipeline().addLast(LOGGING_HANDLER);
+                    ch.pipeline().addLast(MESSAGE_CODEC);
+                    //添加业务处理handler
+                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            //新建一个线程用来接受用户的输入
+                            new Thread(()->{
+                                Scanner scanner = new Scanner(System.in);
+                                System.out.println("请输入用户名：");
+                                String username = scanner.nextLine();
+                                System.out.println("请输入密码");
+                                String password = scanner.nextLine();
+                                LoginRequestMessage message = new LoginRequestMessage();
+                                message.setUsername(username);
+                                message.setPassword(password);
+                                //发送消息
+                                ctx.writeAndFlush(message);
+
+                                //等待输入
+                                try {
+                                    countDownLatch.await();
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                //如果登录失败就退出
+                                if (!LOGIN_FLAG.get()){
+                                    //退出
+                                    ctx.channel().close();
+                                    return;
+                                }
+
+                                while (true){
+                                    System.out.println("=============================");
+                                    System.out.println("send [username] [content]");
+                                    System.out.println("gsend [group name] [content]");
+                                    System.out.println("gcreate [group name] [m1,m2,m3...]");
+                                    System.out.println("gmembers [group name]");
+                                    System.out.println("gjoin [group name]");
+                                    System.out.println("gquit [group name]");
+                                    System.out.println("quit");
+                                    System.out.println("=============================");
+                                    System.out.println("请输入选项：");
+                                    Scanner sc = new Scanner(System.in);
+                                    String input = sc.nextLine();
+                                }
+                            },"user-input").start();
+                        }
+
+                        //接受响应消息
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            log.info("msg:{}",msg);
+                            //拿到登录结果
+                            if(msg instanceof LoginResponseMessage){
+                                boolean loginFlag = ((LoginResponseMessage) msg).isSuccess();
+                                LOGIN_FLAG.set(loginFlag);
+                            }
+                            countDownLatch.countDown();
+                        }
+                    });
+                }
+            });
+            Channel channel = bootstrap.connect("localhost", 8080).sync().channel();
+            channel.closeFuture().sync();
+        } catch (Exception e) {
+            log.error("client error", e);
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/cn/itcast/server/ChatServer.java
@@ -0,0 +1,64 @@
+package cn.itcast.server;
+
+import cn.itcast.message.LoginRequestMessage;
+import cn.itcast.message.LoginResponseMessage;
+import cn.itcast.protocol.MessageCodecSharable;
+import cn.itcast.protocol.ProcotolFrameDecoder;
+import cn.itcast.server.service.UserServiceFactory;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ChatServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+        MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new ProcotolFrameDecoder());
+                    ch.pipeline().addLast(LOGGING_HANDLER);
+                    ch.pipeline().addLast(MESSAGE_CODEC);
+                    ch.pipeline().addLast(new SimpleChannelInboundHandler<LoginRequestMessage>() {
+                        //接受客户端发送过来的消息
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext ctx, LoginRequestMessage msg) throws Exception {
+                            boolean flag = UserServiceFactory.getUserService().login(msg.getUsername(), msg.getPassword());
+                            LoginResponseMessage message;
+                            if (flag) {
+                                message = new LoginResponseMessage(true, "登录成功");
+                                ctx.writeAndFlush(message);
+                            } else {
+                                message = new LoginResponseMessage(false, "登录失败");
+                                ctx.writeAndFlush(message);
+                            }
+                        }
+                    });
+                }
+            });
+            Channel channel = serverBootstrap.bind(8080).sync().channel();
+            channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
